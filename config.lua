74d39fe772b58d9912d319ab1c9597dae0f3f55f
fisken01 <127223475+fisken01@users.noreply.github.com>
2024-01-19 19:35:59 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,415 @@
+Config = {}
+
+Config.Item = 'lockpick'
+
+Config.Cooldown = 600 -- seconds
+
+Config.SearchTime = 5 -- seconds
+
+Config.Houses = {
+
+
+    -- MIRROR PARK
+    [1] = {
+        entry = vector3(1386.3147, -593.4901, 74.4855),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [2] = {
+        entry = vector3(1389.0790, -569.4825, 74.4965),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [3] = {
+        entry = vector3(1373.2882, -555.7859, 74.6857),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [4] = {
+        entry = vector3(1348.3923, -546.7395, 73.8916),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [5] = {
+        entry = vector3(1367.3154, -606.5935, 74.7109),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [6] = {
+        entry = vector3(1341.3358, -597.3080, 74.7008),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [7] = {
+        entry = vector3(1323.4161, -583.2453, 73.2464),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [8] = {
+        entry = vector3(1301.0692, -574.3115, 71.7323),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [9] = {
+        entry = vector3(1328.5554, -536.0288, 72.4409),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [10] = {
+        entry = vector3(1303.1071, -527.3128, 71.4607),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+
+
+    -- GROVE STREET
+    [11] = {
+        entry = vector3(114.4142, -1961.1700, 21.3342),
+        lockpick = 'lockpick' -- navnet på den type lockpick man skal bruge
+    },
+    [12] = {
+        entry = vector3(85.9935, -1959.6948, 21.1217),
+        lockpick = 'lockpick'
+    },
+    [13] = {
+        entry = vector3(76.3230, -1948.1134, 21.1741),
+        lockpick = 'lockpick'
+    },
+    [14] = {
+        entry = vector3(72.1238, -1939.0149, 21.3690),
+        lockpick = 'lockpick'
+    },
+    [15] = {
+        entry = vector3(126.7823, -1929.9722, 21.3824),
+        lockpick = 'lockpick'
+    },
+    [16] = {
+        entry = vector3(118.4318, -1920.9611, 21.3233),
+        lockpick = 'lockpick'
+    },
+    [17] = {
+        entry = vector3(100.9586, -1912.1909, 21.4072),
+        lockpick = 'lockpick'
+    },
+    [18] = {
+        entry = vector3(56.5150, -1922.7061, 21.9111),
+        lockpick = 'lockpick'
+    },
+    [19] = {
+        entry = vector3(38.9109, -1911.5614, 21.9535),
+        lockpick = 'lockpick'
+    },
+    [20] = {
+        entry = vector3(23.1514, -1896.7986, 22.9659),
+        lockpick = 'lockpick'
+    },
+    [21] = {
+        entry = vector3(54.4476, -1873.1353, 22.8056),
+        lockpick = 'lockpick'
+    },
+    [22] = {
+        entry = vector3(45.9557, -1864.1969, 23.2783),
+        lockpick = 'lockpick'
+    },
+    [23] = {
+        entry = vector3(23.0282, -1896.7092, 22.9659),
+        lockpick = 'lockpick'
+    },
+    [24] = {
+        entry = vector3(29.9649, -1854.6600, 24.0688),
+        lockpick = 'lockpick'
+    },
+    [25] = {
+        entry = vector3(5.2744, -1884.2827, 23.6973),
+        lockpick = 'lockpick'
+    },
+    [26] = {
+        entry = vector3(21.4069, -1844.5408, 24.6017),
+        lockpick = 'lockpick'
+    },
+    [27] = {
+        entry = vector3(-4.7092, -1872.1560, 24.1510),
+        lockpick = 'lockpick'
+    },
+    [28] = {
+        entry = vector3(-20.5006, -1858.9930, 25.4087),
+        lockpick = 'lockpick'
+    },
+    [29] = {
+        entry = vector3(-34.2032, -1846.9918, 26.1935),
+        lockpick = 'lockpick'
+    },
+    [30] = {
+        entry = vector3(-41.9291, -1792.1782, 27.8282),
+        lockpick = 'lockpick'
+    },
+    [31] = {
+        entry = vector3(-50.4222, -1783.2896, 28.3008),
+        lockpick = 'lockpick'
+    },
+    [32] = {
+        entry = vector3(104.0367, -1885.3181, 24.3186),
+        lockpick = 'lockpick'
+    },
+    [33] = {
+        entry = vector3(115.4879, -1887.8538, 23.9282),
+        lockpick = 'lockpick'
+    },
+    [34] = {
+        entry = vector3(128.2771, -1896.9451, 23.6742),
+        lockpick = 'lockpick'
+    },
+    [35] = {
+        entry = vector3(148.7264, -1904.4689, 23.5316),
+        lockpick = 'lockpick'
+    },
+    [36] = {
+        entry = vector3(179.3211, -1923.9174, 21.3710),
+        lockpick = 'lockpick'
+    },
+    [37] = {
+        entry = vector3(165.0907, -1945.0339, 20.2356),
+        lockpick = 'lockpick'
+    },
+    [38] = {
+        entry = vector3(148.9135, -1960.5439, 19.4589),
+        lockpick = 'lockpick'
+    },
+    [39] = {
+        entry = vector3(148.9290, -1960.5713, 19.4588),
+        lockpick = 'lockpick'
+    },
+    [40] = {
+        entry = vector3(144.1698, -1968.8923, 18.8576),
+        lockpick = 'lockpick'
+    },
+    [41] = {
+        entry = vector3(130.6150, -1853.2649, 25.2349),
+        lockpick = 'lockpick'
+    },
+    [42] = {
+        entry = vector3(150.0246, -1864.6942, 24.5912),
+        lockpick = 'lockpick'
+    },
+    [43] = {
+        entry = vector3(171.4992, -1871.6324, 24.4002),
+        lockpick = 'lockpick'
+    },
+    [44] = {
+        entry = vector3(192.2294, -1883.3102, 25.0566),
+        lockpick = 'lockpick'
+    },
+    [45] = {
+        entry = vector3(208.5558, -1895.3081, 24.8142),
+        lockpick = 'lockpick'
+    },
+    [46] = {
+        entry = vector3(250.9941, -2030.2216, 18.7061),
+        lockpick = 'lockpick'
+    },
+    [47] = {
+        entry = vector3(236.0732, -2046.4340, 18.3800),
+        lockpick = 'lockpick'
+    },
+    [48] = {
+        entry = vector3(251.0032, -2030.2050, 18.7061),
+        lockpick = 'lockpick'
+    },
+    [49] = {
+        entry = vector3(256.3515, -2023.4932, 19.2663),
+        lockpick = 'lockpick'
+    },
+    [50] = {
+        entry = vector3(279.6480, -1993.9854, 20.8038),
+        lockpick = 'lockpick'
+    },
+    [51] = {
+        entry = vector3(291.5614, -1980.1019, 21.6005),
+        lockpick = 'lockpick'
+    },
+    [52] = {
+        entry = vector3(295.7399, -1971.8745, 22.9007),
+        lockpick = 'lockpick'
+    },
+
+
+
+    -- VAGOS BLOKKEN
+    [53] = {
+        entry = vector3(332.4651, -2018.4695, 22.3541),
+        lockpick = 'lockpick'
+    },
+    [54] = {
+        entry = vector3(336.1248, -2021.5372, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [55] = {
+        entry = vector3(343.5251, -2027.7377, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [56] = {
+        entry = vector3(344.7469, -2028.8870, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [57] = {
+        entry = vector3(352.0907, -2035.0094, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [58] = {
+        entry = vector3(353.6546, -2036.2596, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [59] = {
+        entry = vector3(360.8221, -2042.2867, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [60] = {
+        entry = vector3(364.5911, -2045.5042, 22.3543),
+        lockpick = 'lockpick'
+    },
+    [61] = {
+        entry = vector3(371.3538, -2057.2690, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [62] = {
+        entry = vector3(372.4265, -2055.9866, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [63] = {
+        entry = vector3(365.1232, -2064.6941, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [64] = {
+        entry = vector3(363.9432, -2066.1050, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [65] = {
+        entry = vector3(356.7288, -2074.7048, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [66] = {
+        entry = vector3(357.9167, -2073.2891, 21.7445),
+        lockpick = 'lockpick'
+    },
+    [67] = {
+        entry = vector3(345.4269, -2067.5437, 20.9367),
+        lockpick = 'lockpick'
+    },
+    [68] = {
+        entry = vector3(341.8185, -2064.5156, 20.9367),
+        lockpick = 'lockpick'
+    },
+    [69] = {
+        entry = vector3(334.5190, -2058.2683, 20.9320),
+        lockpick = 'lockpick'
+    },
+    [70] = {
+        entry = vector3(333.0954, -2057.2065, 20.9364),
+        lockpick = 'lockpick'
+    },
+    [71] = {
+        entry = vector3(325.8243, -2051.0913, 20.9364),
+        lockpick = 'lockpick'
+    },
+    [72] = {
+        entry = vector3(324.3912, -2049.8474, 20.9364),
+        lockpick = 'lockpick'
+    },
+    [73] = {
+        entry = vector3(317.1094, -2043.7468, 20.9364),
+        lockpick = 'lockpick'
+    },
+    [74] = {
+        entry = vector3(313.4601, -2040.6028, 20.9362),
+        lockpick = 'lockpick'
+    },
+    [75] = {
+        entry = vector3(297.8134, -2034.6545, 19.8358),
+        lockpick = 'lockpick'
+    },
+    [76] = {
+        entry = vector3(293.3136, -2044.1454, 19.6463),
+        lockpick = 'lockpick'
+    },
+    [77] = {
+        entry = vector3(290.6691, -2047.2975, 19.6463),
+        lockpick = 'lockpick'
+    },
+    [78] = {
+        entry = vector3(286.3063, -2052.4973, 19.6463),
+        lockpick = 'lockpick'
+    },
+
+
+    -- Vespucci Canals
+    [79] = {
+        entry = vector3(-1068.0157, -1163.5970, 2.7453),
+        lockpick = 'lockpick'
+    },
+    [80] = {
+        entry = vector3(-1074.1460, -1152.7045, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [81] = {
+        entry = vector3(-1063.4958, -1160.3053, 2.7464),
+        lockpick = 'lockpick'
+    },
+    [82] = {
+        entry = vector3(-1045.8318, -1159.8159, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [83] = {
+        entry = vector3(-1063.8701, -1133.5466, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [84] = {
+        entry = vector3(-1034.7750, -1147.2509, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [85] = {
+        entry = vector3(-1040.4045, -1135.8823, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [86] = {
+        entry = vector3(-1024.3302, -1139.8835, 2.7459),
+        lockpick = 'lockpick'
+    },
+    [87] = {
+        entry = vector3(-1031.8015, -1109.4264, 2.1586),
+        lockpick = 'lockpick'
+    },
+    [88] = {
+        entry = vector3(-985.8273, -1121.7294, 4.5456),
+        lockpick = 'lockpick'
+    },
+    [89] = {
+        entry = vector3(-991.7051, -1103.4735, 2.1503),
+        lockpick = 'lockpick'
+    },
+    [90] = {
+        entry = vector3(-978.1028, -1108.3875, 2.1503),
+        lockpick = 'lockpick'
+    },
+    [91] = {
+        entry = vector3(-977.7484, -1092.2938, 4.2226),
+        lockpick = 'lockpick'
+    },
+    [92] = {
+        entry = vector3(-967.4602, -1080.3945, 2.1718),
+        lockpick = 'lockpick'
+    },
+    [93] = {
+        entry = vector3(-956.6567, -1105.4772, 2.1503),
+        lockpick = 'lockpick'
+    },
+    [94] = {
+        entry = vector3(-948.4254, -1107.6302, 2.1718),
+        lockpick = 'lockpick'
+    },
+    [95] = {
+        entry = vector3(-952.4916, -1077.5809, 2.6737),
+        lockpick = 'lockpick'
+    },
+    [96] = {
+        entry = vector3(-938.6191, -1087.9122, 2.1503),
+        lockpick = 'lockpick'
+    },
+    [97] = {
+        entry = vector3(-943.2794, -1075.4341, 2.7453),
+        lockpick = 'lockpick'
+    },
+    
+    
+
+    
+}
